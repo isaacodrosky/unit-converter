bc6999a0419c6d26fbcab19cc47d126c3fbf7ac2
isaacodrosky <isaacodrosky@gmail.com>
2023-04-20 21:00:40 -0400

    Syntax fixes

--- index.js
+++ index.js
@@ -1,26 +1,34 @@
 // HTML Elements
-const inputEl = document.getElementById("input-el")
-const convertBtn = document.getElementById("convert-btn")
-const lengthEl = document.getElementById("length-el")
-const volumeEl = document.getElementById("volume-el")
-const massEl = document.getElementById("mass-el")
+const inputEl = document.getElementById("input-el");
+const convertBtn = document.getElementById("convert-btn");
+const lengthEl = document.getElementById("length-el");
+const volumeEl = document.getElementById("volume-el");
+const massEl = document.getElementById("mass-el");
 
 // Conversion Rates
-const metersToFeet = 3.281
-const litersToGallons = 0.264
-const kilogramsToPounds = 2.204
+const metersToFeet = 3.281;
+const litersToGallons = 0.264;
+const kilogramsToPounds = 2.204;
 
-convertBtn.addEventListener("click", function() {
-    // store user input in variable - ensure typeof = number
-    const userInput = Number(inputEl.value)
-    // set each section to display user input number and converted number, rounded to 3 decimal places
-    lengthEl.textContent = `
+convertBtn.addEventListener("click", function () {
+  // store user input in variable - ensure typeof = number
+  const userInput = Number(inputEl.value);
+  // set each section to display user input number and converted number, rounded to 3 decimal places
+  lengthEl.textContent = `
         ${userInput} meters = ${(userInput * metersToFeet).toFixed(3)} feet | 
-        ${userInput} feet = ${(userInput / metersToFeet).toFixed(3)} meters`
-    volumeEl.textContent = `
-        ${userInput} liters = ${(userInput * litersToGallons).toFixed(3)} gallons | 
-        ${userInput} gallons = ${(userInput / litersToGallons).toFixed(3)} liters`
-    massEl.textContent = `
-        ${userInput} kilograms = ${(userInput * kilogramsToPounds).toFixed(3)} pounds | 
-        ${userInput} pounds = ${(userInput / kilogramsToPounds).toFixed(3)} kilograms`
-})
+        ${userInput} feet = ${(userInput / metersToFeet).toFixed(3)} meters`;
+  volumeEl.textContent = `
+        ${userInput} liters = ${(userInput * litersToGallons).toFixed(
+    3
+  )} gallons | 
+        ${userInput} gallons = ${(userInput / litersToGallons).toFixed(
+    3
+  )} liters`;
+  massEl.textContent = `
+        ${userInput} kilograms = ${(userInput * kilogramsToPounds).toFixed(
+    3
+  )} pounds | 
+        ${userInput} pounds = ${(userInput / kilogramsToPounds).toFixed(
+    3
+  )} kilograms`;
+});
